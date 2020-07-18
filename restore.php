f4a23cc157fd8f4414ae5e9bbf5ae292f170a7d9
Ilya K <kis3lewil@yandex.ru>
2020-07-18 14:52:29 +0300

    modify table

--- restore.php
+++ restore.php
@@ -19,45 +19,47 @@ $statementPDO = $pdo->exec('CREATE TABLE ' . dataTable .
 
 $statementPDO = $pdo->exec('CREATE TABLE ' . userTable .
 '( id int(11) NOT NULL AUTO_INCREMENT,
-  user_id int(11) NOT NULL,
   email VARCHAR(254) NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   middle_name VARCHAR(30) NOT NULL,
   city VARCHAR(40) NOT NULL,
   vk VARCHAR(32) NOT NULL,
-  FOREIGN KEY(id) REFERENCES data(id),
+  FOREIGN KEY(id) REFERENCES ' . dataTable . '(id),
   PRIMARY KEY(id)
 )');
 
 $statementPDO = $pdo->exec('CREATE TABLE ' . shopsTable .
 '( id int(11) NOT NULL AUTO_INCREMENT,
-  login VARCHAR(32) NOT NULL,
-  password VARCHAR(64) NOT NULL,
-  PRIMARY KEY(id),
-  UNIQUE KEY `login` (`login`)
-)');
-
-$statementPDO = $pdo->exec('CREATE TABLE ' . cityTable .
-'( id int(11) NOT NULL AUTO_INCREMENT,
-  shop VARCHAR(90) NOT NULL,
+  shop VARCHAR(32) NOT NULL,
   PRIMARY KEY(id)
 )');
 
-$statementPDO = $pdo->exec('CREATE TABLE ' . chatMessageTable .
+$statementPDO = $pdo->exec('CREATE TABLE ' . cityTable .
 '( id int(11) NOT NULL AUTO_INCREMENT,
   city_translit VARCHAR(90) NOT NULL,
   city_rus VARCHAR(90) NOT NULL,
   PRIMARY KEY (id)
 )');
 
+
 $statementPDO = $pdo->exec('CREATE TABLE ' . chatInfoTable .
+'( id INT(11) NOT NULL AUTO_INCREMENT,
+  shop_id INT(11) NOT NULL,
+  name VARCHAR(64) NOT NULL,
+  chat_id INT(5) NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(shop_id) REFERENCES ' . shopsTable . '(id)
+)');
+
+$statementPDO = $pdo->exec('CREATE TABLE ' . chatMessageTable .
 '( userMessageId int(11) NOT NULL AUTO_INCREMENT,
-  login VARCHAR(64) NOT NULL,
-  message blob NOT NULL,
-  posted_at datetime NOT NULL,
+  user_id INT(11) NOT NULL,
+  message BLOB NOT NULL,
+  posted_at DATETIME NOT NULL,
   chat_id VARCHAR(5) NOT NULL,
-  PRIMARY KEY(userMessageId)
+  PRIMARY KEY(userMessageId),
+  FOREIGN KEY(user_id) REFERENCES ' . dataTable . '(id)
 )');
 
 ?>
